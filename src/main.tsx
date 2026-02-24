e5967366a42b72075c11332eda74e4574e402a1f
JamieOTR <theriseotr@gmail.com>
2026-02-24 08:44:33 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { initWebVitals } from "./observability/webVitals";
-initWebVitals();
-import { initSentry } from "./observability/sentry";
-initSentry();
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initWebVitals } from "./observability/webVitals";
+import { initSentry } from "./observability/sentry";
+
+initSentry();
+initWebVitals();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
